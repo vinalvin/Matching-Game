1ed955614ca3f8da9d2af951648a9ea6139cd76c
Junming Zhang <vinalvin1010@gmail.com>
2019-08-22 21:23:52 -0700

    I changed the key to Mikos one

--- assets/javascript/game-alvin.js
+++ assets/javascript/game-alvin.js
@@ -1,40 +1,49 @@
-//firebase with API
 var firebaseConfig = {
-    apiKey: "AIzaSyBjHkuiXwX8NF68P9suoG15wwn8T24Z0NQ",
-    authDomain: "matching-fun-game.firebaseapp.com",
-    databaseURL: "https://matching-fun-game.firebaseio.com",
-    projectId: "matching-fun-game",
+    apiKey: "AIzaSyBIhWAbx75eQKlBdJrHjhrWaj6Jzk4OvXo",
+    authDomain: "project-f384a.firebaseapp.com",
+    databaseURL: "https://project-f384a.firebaseio.com",
+    projectId: "project-f384a",
     storageBucket: "",
-    messagingSenderId: "793782454862",
-    appId: "1:793782454862:web:4223b1cc2cde498b"
+    messagingSenderId: "554667680468",
+    appId: "1:554667680468:web:6ae2bccf17864595"
 };
-
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var db = firebase.database()
-console.log(db)
-
-
 var formName;
-var score;
-var level;
 
 function pushName() {
     formName = $("#userName").val();
     score = $("#score").text();
-    level = $("#level").text();
-    
     db.ref().push({
         name: formName,
         score: score,
-        level: level, 
     })
 }
-
 // $(document).on("click", "#btnSubmit", function() {
 // pushName()
 
 // })
+
+
+// // Firebase watcher .on("child_added"
+// database.ref().orderByChild("counter").on("child_added", function(snapshot, prevChildKey) {
+//     // storing the snapshot.val() in a variable for convenience
+//     var sv = snapshot.val();
+
+//     var $tbody = $('#highScores tbody');
+
+//     var $tr = $('<tr>');
+//     var $tdName = $('<td>').text(sv.userName);
+//     var $tdCounter = $('<td>').text(sv.counter);
+//     var $tdScore = $('<td>').text(sv.score);
+//     var $tdDifficulty = $('<td>').text(sv.difficulty);
+//     var $tdDateAdded = $('<td>').text(new Date(sv.dateAdded));
+
+//     $tr.append($tdName, $tdCounter, $tdScore, $tdDifficulty, $tdDateAdded);
+
+//     $tbody.append($tr);
+// });
 
 // function pushScoreToFireBase() {
 //     database.ref().push({
@@ -46,15 +55,15 @@ function pushName() {
 //     });
 // }
 
-
+// Timer;
 var counter = 30;
 var $span = $('#timer');
 var alertedScore = false;
+
 var interval;
 
 $span.text(counter);
 
-// Countdown timer
 function decreaseTime() {
     if ((counter == 0) && (alertedScore == false)) {
         alert('Your score is ' + score);
@@ -67,7 +76,6 @@ function decreaseTime() {
     if (counter == -1) {
         $span.text('Time is up');
         clearInterval(interval);
-       
         pushName();
     }
 }
@@ -79,19 +87,14 @@ var userName;
 var rows = 3;
 var cols = 4;
 
-
-$('#btnSubmit').on('click', function (event) {
-    
-    event.preventDefault();
+$('button').on('click', function() {
     // Input the user names;
     userName = $('#userName').val();
 
     interval = setInterval(decreaseTime, 1 * 1000);
-    
     // Different Levels;
     var level = $('#level').val()
     console.log(level)
-    
     if (level === 'easy') {
         rows = 3;
         cols = 4;
@@ -103,62 +106,41 @@ $('#btnSubmit').on('click', function (event) {
         cols = 5;
     }
 
-    // var imageUrls = [
-    //     'https://media0.giphy.com/media/l0MYNJC1oGwKLfqso/200.webp?cid=790b7611d34c1c000e51269097b60ed2641e1d871812a2fc&rid=200.webp',
-    //     'https://media2.giphy.com/media/UtvkbCvOJCJtC/200w.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200w.webp',
-    //     'https://media2.giphy.com/media/k8kA7AOLFVohq/200.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200.webp',
-    //     'https://media1.giphy.com/media/cQz5MLlnP5rfa/200.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200.webp',
-    //     'https://media3.giphy.com/media/9MraLzmkWiZqM/200w.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200w.webp',
-    //     'https://media3.giphy.com/media/3ov9k4e03yTNRWTgYM/200w.webp?cid=790b7611fc7ad1db280bdc6f9bb012ccc148ab3e0adda710&rid=200w.webp',
-    //     'https://media0.giphy.com/media/RHJjogamfp4Qm4D9pz/giphy.gif?cid=ecf05e47317383e20249b53a927d22a3f3625315e480e5d2&tid=a632eba0c20cffca6be46e41b0b923982d23f91a95cfd90b2181e30262e60d9d&rid=giphy.gif',
-    //     'https://media2.giphy.com/media/lOmoovGKkQet9N4uJw/giphy.gif?cid=790b761126043efbe3fc870e111cda1fc0f0733abc621772&rid=giphy.gif',
-    //     'https://media3.giphy.com/media/L2Gu7sMT7kXR13acn7/giphy.gif?cid=790b7611f15b7653c85e8a769ce525c802a31e7380ecdbda&rid=giphy.gif',
-    //     'https://media2.giphy.com/media/26DONKguJRB1vZsAM/giphy.gif?cid=790b76118f4e0bee8a27468831bae75a099a9f6f78c6649c&rid=giphy.gif'
-    // ];
-    var imageUrls = 'https://api.giphy.com/v1/gifs/trending?api_key=N0CBgF93RZv7210eKzxfG7V8BhLtcqg0&limit=10';
+    var imageUrls = [
+        'https://media0.giphy.com/media/l0MYNJC1oGwKLfqso/200.webp?cid=790b7611d34c1c000e51269097b60ed2641e1d871812a2fc&rid=200.webp',
+        'https://media2.giphy.com/media/UtvkbCvOJCJtC/200w.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200w.webp',
+        'https://media2.giphy.com/media/k8kA7AOLFVohq/200.webp?cid=790b7611361e9dda43fa5d4ceadefc1d6ffba77218c5b525&rid=200.webp',
+        'https://media1.giphy.com/media/cQz5MLlnP5rfa/200.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200.webp',
+        'https://media3.giphy.com/media/9MraLzmkWiZqM/200w.webp?cid=790b7611561f26b842f49393a2fbf4e32a0542969b591acd&rid=200w.webp',
+        'https://media3.giphy.com/media/3ov9k4e03yTNRWTgYM/200w.webp?cid=790b7611fc7ad1db280bdc6f9bb012ccc148ab3e0adda710&rid=200w.webp',
+        'https://media0.giphy.com/media/RHJjogamfp4Qm4D9pz/giphy.gif?cid=ecf05e47317383e20249b53a927d22a3f3625315e480e5d2&tid=a632eba0c20cffca6be46e41b0b923982d23f91a95cfd90b2181e30262e60d9d&rid=giphy.gif',
+        'https://media2.giphy.com/media/lOmoovGKkQet9N4uJw/giphy.gif?cid=790b761126043efbe3fc870e111cda1fc0f0733abc621772&rid=giphy.gif',
+        'https://media3.giphy.com/media/L2Gu7sMT7kXR13acn7/giphy.gif?cid=790b7611f15b7653c85e8a769ce525c802a31e7380ecdbda&rid=giphy.gif',
+        'https://media2.giphy.com/media/26DONKguJRB1vZsAM/giphy.gif?cid=790b76118f4e0bee8a27468831bae75a099a9f6f78c6649c&rid=giphy.gif'
+    ];
+    // var imageUrls = 'https://api.giphy.com/v1/gifs/random?api_key=N0CBgF93RZv7210eKzxfG7V8BhLtcqg0';
+
     var imageIdxs = [];
-    var images = [];
+    for (var idx = 0; idx < rows * cols; idx++) {
+        imageIdxs.push(Math.floor(idx / 2));
+    }
+    imageIdxs = shuffle(imageIdxs);
 
-    // for (var idx = 0; idx < rows * cols; idx++) {
-    //     imageIdxs.push(Math.floor(idx / 2));
-    // }
-    // imageIdxs = shuffle(imageIdxs);
-
-    function pushImages() {
-        $.ajax({
-            url: imageUrls,
-            method: 'GET'
-        }).then(function(response) {
-            for (var i=0; i<response.data.length; i++) {
-                // console.log(response.data[i].images.downsized_large.url)
-                images.push(response.data[i].images.downsized_large.url)
-                imageIdxs.push(i);
-            }
-            
-            // imageIdxs.push(response.data.images.downsized_large.url);
-            // console.log(rows * cols + ':' + imageIdxs.length);
-
-            // if (imageIdxs.length == rows * cols) {
-            //     callback();
-            // }
-            var arr = [1,2,3,4,5]
-            console.log(response)
-            console.log(images)
-            console.log(shuffle(images))
-        })
-        
-    };
-    pushImages()
+    // function pushImages() {
+    //     $.ajax({
+    //         url: imageUrls,
+    //         method: 'GET'
+    //     }).then(function(response) {
+    //         imageIdxs.push(response.data.images.downsized_large.url);
+    //     })
+    // };
 
     var $container = $('.container');
     for (var row = 0; row < rows; row++) {
-        var $cardRowElem = $('<div class="row justify-content-center"></div>');
-        
+        var $cardRowElem = $('<div class="row"></div>');
         for (var col = 0; col < cols; col++) {
             var idx = row * cols + col;
-
-            //*** try to solve by adding the images links into src ****** look at line 160, 120
-            var $card = $('<div class="imgContainer mx-1"><img class="hide" src="' + images[imageIdxs[idx]] + '"></div>');
+            var $card = $('<div class="imgContainer"><img class="hide" src="' + imageUrls[imageIdxs[idx]] + '"></div>');
             $cardRowElem.append($card);
         }
         $container.append($cardRowElem);
@@ -196,14 +178,12 @@ function shuffle(array) {
         array[randomIndex] = temporaryValue;
     }
     return array;
-
 };
 
 var $firstImg;
 var $secondImg;
 
-
-$(document).on('click', '.imgContainer', function () {
+$(document).on('click', '.imgContainer', function() {
 
     // If either one is empty;
     if ((firstSrc == undefined) || (secondSrc == undefined)) {
@@ -226,7 +206,7 @@ $(document).on('click', '.imgContainer', function () {
             $secondImg = $img;
         }
         // If the last two images clicked on match, it would keep them turned over; else, keep them hidden; 
-        var timer = setTimeout(function () {
+        var timer = setTimeout(function() {
 
             // Both are not empty;
             if ((firstSrc != undefined) && (secondSrc != undefined)) {
@@ -246,7 +226,6 @@ $(document).on('click', '.imgContainer', function () {
 
                         alertedScore = true;
                         pushName();
-
                         // Let the timer stops;
                         clearInterval(interval);
                     }
